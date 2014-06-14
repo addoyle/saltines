37719ceb1e7cf58c8bd12d370532861aaa1eb95d
georgeh12 <georgeh12@gmail.com>
2014-06-14 04:20:34 -0400

    added mysql connection

--- server/web.js
+++ server/web.js
@@ -10,6 +10,24 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(require('express-ejs-layouts'))
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '192.168.25.199', //172.16.240.20
+  port     : 3306,
+  user     : 'Fishack',
+  password : 'P@$$w0rd'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 app.get('/test', function(req, res) {
 	res.render('index');
 });
